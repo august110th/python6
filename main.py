307af4a2f06693199579c99c1fee9cdbcd031d60
august110th <80058477+august110th@users.noreply.github.com>
2021-06-21 11:10:02 +0400

    done

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ if not response.ok:
 
 text = response.text
 soup = BeautifulSoup(text, features='html.parser')
-KEYWORDS = {'дизайн', 'фото', 'web', 'python'}
-
+KEYWORDS = {'дизайн', 'фото', 'web', 'python', 'полнота'}
 articles = soup.find_all('article')
 
 for i in articles:
-    title = {i.find('a', class_='post__title_link').text}
-    data = i.find('span',class_="post__time" ).text
-    link = i.find('a', class_='post__title_link', href = True)
-    if KEYWORDS & title:
+    title = i.find('a', class_='post__title_link').text
+    data = i.find('span', class_="post__time").text
+    link = i.find('a', class_='post__title_link', href=True)
+    if KEYWORDS & set((title.lower()).split()):
         print(title)
         print(data)
         print(link['href'])
